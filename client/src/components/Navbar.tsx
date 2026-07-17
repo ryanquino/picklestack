b943f043c99c7e478385d889728fc0353769115c
ryanquino <33245934+ryanquino@users.noreply.github.com>
2026-07-17 23:23:51 +0800

    main
    updated logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <nav className="navbar" aria-label="Main navigation">
       <Link to="/" className="navbar__brand">
-        <img src="/logo-light.png" alt="Pickld logo" className="navbar__logo" />
+        <img src="/logo.png" alt="PaddleFlux logo" className="navbar__logo" />
       </Link>
 
       <div className="navbar__links">
